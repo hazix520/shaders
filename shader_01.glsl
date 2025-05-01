f39ce4651a3054d8793be099a030312333b7c899
hazix520 <78504489+hazix520@users.noreply.github.com>
2025-04-30 21:49:25 -0400

    Add files via upload

---
+++ shader_01.glsl
@@ -0,0 +1,16 @@
+void main () {
+
+vec2 uv = uv();
+
+vec3 c = black;
+
+
+float x = noise(uv);
+float y = vrmf(rotate(uv, vec2(0.2,0.2), time), 2);
+
+c += sin(blue * sin(time) * x * 10./(y * sin(time /4.)));
+c+= sin(red * y * sin(time) * 2.);
+
+
+gl_FragColor = vec4(c,1.);
+}
